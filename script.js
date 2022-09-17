45ca6ec5023a26675a64e00147f6663d27972de5
Ayodele hazzan <taiwoayodele180@gmail.com>
2022-09-17 09:01:28 +0100

    this is my second  git commit

---
+++ script.js
@@ -0,0 +1,36 @@
+//Created By Hazzan Tech
+
+document.getElementById("submit").onclick = function(){
+    let d1 = document.getElementById("date").value;
+    let m1 = document.getElementById("month").value;
+    let y1 = document.getElementById("year").value;
+
+    d1 = Number(d1);
+    m1 = Number(m1);
+    y1 = Number(y1);
+
+    date = new Date();
+    let d2 = date.getDate();
+    let m2 = date.getMonth() + 1;
+    let y2 = date.getFullYear();
+    let month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    if(d1 > d2){
+        d2 = d2 + month[m2 - 1];
+        m2 = m2 - 1;
+    }
+
+    if(m1 > m2){
+        m2 = m2 + 12;
+        y2 = y2 - 1;
+    }
+
+    let d = d2 - d1;
+    let m = m2 - m1;
+    let y = y2 - y1;
+
+    document.getElementById("display").innerHTML = `Your Age Is ${y} Years, ${m} Month and ${d} Days`;
+    
+}
+
+//Created By Hazzan Tech
